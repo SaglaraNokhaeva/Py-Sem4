280e3e09215b604ba2747e7f3e4db4fac94cbc30
Saglara <nohaevass@yandex.ru>
2022-12-04 15:01:06 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,15 +2,15 @@
 # Окончанием ввода пусть служит пустая строка.
 
 
-data = open('file.txt', 'a', encoding='UTF-8')
+# data = open('file.txt', 'a', encoding='UTF-8')
 
-while True:
-    string = input('Введите строку: ')
-    if string == '':
-        break
-    data.write(string + '\n')
+# while True:
+#     string = input('Введите строку: ')
+#     if string == '':
+#         break
+#     data.write(string + '\n')
 
-data.close()
+# data.close()
 
 
 # Задача 2. В текстовом файле посчитать количество строк, а также для каждой отдельной строки определить количество 
@@ -20,8 +20,8 @@ data.close()
 data = open('file.txt', 'r', encoding='UTF-8')
 
 for s in data.readlines():
-print(s, end='')
-print(f'Количество символов = {len(s)}')
-print(f"Количество слов = {len(s.split())}")
-print()
+    print(s, end='')
+    print(f'Количество символов = {len(s)}')
+    print(f"Количество слов = {len(s.split())}")
+    print()
 data.close()
